a5333d1b6ac99949c147f528c44bba70e4b9c400
Admin <govindjawade19@gmail.com>
2024-07-08 23:45:44 +0530

    updated

--- src/main/java/PMSLoginPages/ActiveLinks.java
+++ src/main/java/PMSLoginPages/ActiveLinks.java
@@ -13,6 +13,9 @@ public class ActiveLinks {
     @FindBy(xpath = "//ul/li/a[contains(.,'Manage Global Master Files')]")
     WebElement globalmasterfiles;
 
+    @FindBy(xpath = "//li/a[contains(.,'Entity Setup')]")
+    WebElement entitySetUps;
+
     public ActiveLinks(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
@@ -21,19 +24,32 @@ public class ActiveLinks {
     public void activeLinkLeftMenu() {
 
         List<WebElement> MenuLinks = driver.findElements(By.xpath("//a[contains(@class,'nav-link')]"));
-        System.out.println("ActiveLinksInLeftMenu " + MenuLinks.size());
+        System.out.println("ActiveLinksInLeftMenu ---------->>>: " + MenuLinks.size());
         for (WebElement menuLink : MenuLinks) {
             System.out.println(menuLink.getText());
         }
+
     }
 
     public void activeLinkMasterFiles() throws InterruptedException {
         Thread.sleep(4000);
         globalmasterfiles.click();
+        Thread.sleep(4000);
         List<WebElement> MastersLink = driver.findElements(By.xpath("//ul/li/a[contains(@href,'manage-global-master-files')]"));
-        System.out.println("ActiveLinksInGlobalMastreFiles " + MastersLink.size());
+        System.out.println("ActiveLinksInGlobalMastreFiles ----------->>>: " + MastersLink.size());
         for (int i = 0; i < MastersLink.size(); i++) {
             System.out.println(MastersLink.get(i).getText());
+        }
+    }
+
+    public void activeLinkEntitySetFiles() throws InterruptedException {
+        Thread.sleep(4000);
+        entitySetUps.click();
+        Thread.sleep(4000);
+        List<WebElement> EntiFilesLink = driver.findElements(By.xpath("//ul/li/a[contains(@href,'entity-setup')]"));
+        System.out.println("ActiveLinksInEntitySetFiles---------------->>>:  " + EntiFilesLink.size());
+        for (int i = 0; i < EntiFilesLink.size(); i++) {
+            System.out.println(EntiFilesLink.get(i).getText());
         }
     }
 }
